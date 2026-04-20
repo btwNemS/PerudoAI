3dfda51462039387fe518c7050e4c65a4511e52e
btwNemS <fofi.simon.delporte@gmail.com>
2026-04-20 15:51:50 +0200

    avancement Coeur

--- Coeur.class.php
+++ Coeur.class.php
@@ -1,8 +1,44 @@
 <?php
-class Coeur extends Joueur 
+class Coeur extends Joueur
 {
+  protected $probabilite;
+  protected $nbDesAdverse;
 
+  public function __construct()
+  {
+    $this->probabilite = $this->majTableProbabilite();
+    $this->nbDesAdverse = 15;
+  }
+
+  protected function historique($coupsJoues, $nbDesParJoueur)
+  {
+    $this->nbDesAdverse = array_sum($nbDesParJoueur) - $this->nbDes;
+  }
+  //$coupsJoues est un tableau de l'historique des coups
+  //$nbDesParJoueur est un tableau d'entiers
+
+
+  protected function evaluer($qte, $val, $palifico, $nbDes) {}
+  //$palifico est un booleen, retourne un tableau de 2 cases contenant la nouvelle quantité et la nouvelle valeur
+
+
+  private function majTableProbabilite()
+  {
+    $probabilite = 0;
+    return $probabilite;
+  }
+
+  /**
+   * Explications
+   */
+  private function paireProbabilite($Q, $V, $nbDes)
+  {
+    $probabilitePaire = 0;
+    $z = array_count_values($this->mesDes)[$V] ?? 0;
+    $k = $Q;
+    $y = $k - $z;
+    $x = $this->nbDesAdverse;
+
+    return $probabilitePaire;
+  }
 }
-
-
-?>
